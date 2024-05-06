4f3fb428d60a715a15b3e5ca9bac5f4131421885
hasiu12 <131602193+hasiu12@users.noreply.github.com>
2024-05-06 22:00:07 +0200

    Update inzynierja.js

--- inzynierja.js
+++ inzynierja.js
@@ -24,7 +24,7 @@ async function fetchData() {
             // Pobierz dane z JSONbin.io
             const response = await fetch('https://api.jsonbin.io/v3/b/6475f35a8e4aa6225ea69f8f', {
                 headers: {
-                    'X-Master-Key': '$2b$10$BbsRmIDNnUiQ7WXSyaL.HuG/KKFiqa5.2mnBS2v3nzTA60pusrz1.'
+                    'X-Master-Key': '$2a$10$.3Jff8CL8j1TYeHi0IqhLONpWB6FyRuRN.fWTYflF5km0x5RDXWs2'
                 }
             });
 
